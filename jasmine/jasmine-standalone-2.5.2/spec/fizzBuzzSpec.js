99c483ebf4ee591496f08157045f948b8ff696b8
Stefan Liute <stefanliute@gmail.com>
2017-01-30 16:25:04 +0000

    Third commit - third test - passing 15, expecting 'FizzBuzz'; fourth test - passing any other number

--- jasmine/jasmine-standalone-2.5.2/spec/fizzBuzzSpec.js
+++ jasmine/jasmine-standalone-2.5.2/spec/fizzBuzzSpec.js
@@ -1,10 +1,18 @@
 describe('Fizzbuzz', function(){
 
-  it("returns 'Fizz' when passed the number 3", function() {
+  it("returns 'Fizz' when passed numbers divisible by 3", function() {
     expect(Fizzbuzz(3)).toEqual('Fizz');
   });
 
-  it("returns 'Buzz' when passed the number 5", function() {
+  it("returns 'Buzz' when passed numbers divisible by 5", function() {
     expect(Fizzbuzz(5)).toEqual('Buzz');
+  });
+
+  it("returns 'FizzBuzz' when passed numbers divisible by 15", function() {
+    expect(Fizzbuzz(15)).toEqual('FizzBuzz');
+  });
+
+  it("returns a number not divisible by either 3 or 5", function() {
+    expect(Fizzbuzz(13)).toEqual(13);
   });
 });
--- jasmine/jasmine-standalone-2.5.2/src/fizzBuzz.js
+++ jasmine/jasmine-standalone-2.5.2/src/fizzBuzz.js
@@ -1,8 +1,12 @@
-function Fizzbuzz(number){
-  if ( number % 3 === 0 ) {
+function Fizzbuzz(number) {
+  if ( number % 3 === 0 && number % 5 === 0 ) {
+    return 'FizzBuzz'
+  } else if ( number % 3 === 0 ) {
     return 'Fizz'
-  } else {
+  } else if ( number % 5 === 0 ) {
     return 'Buzz'
+  } else {
+    return number
   }
 
 };
